0b8af289ed8b3f3ff80821343c32f1b99565b060
Thomas Valois <thomas@viscreo.co.uk>
2022-11-24 15:51:54 +0000

    Update server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('${process.env.SERVER_HOST}', '0.0.0.0'),
-  port: env.int('${process.env.SERVER_PORT}', 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   url: 'https://cms.viscreo.co.uk',
   app: {
     keys: env.array("APP_KEYS"),
