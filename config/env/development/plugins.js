40d75f00dc3ca8eb7c19b2646aa4eee4f77db852
Thomas Valois <thomas@viscreo.co.uk>
2022-12-21 14:24:19 +0000

    Removal of unused plugins

--- config/env/development/plugins.js
+++ config/env/development/plugins.js
@@ -18,15 +18,6 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  seo: {
-    enabled: true,
-  },
-  placeholder: {
-    enabled: true,
-    config: {
-      size: 10,
-    }
-  },
   email: {
     config: {
       provider: "sendgrid",
