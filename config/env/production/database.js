aaab84f46b7f6f82639766bab48af7a7847e80de
Thomas Valois <thomas@viscreo.co.uk>
2023-01-11 23:24:33 +0000

    SECURITY FIX --> Upgrade to 4.5.6

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,11 +2,6 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      /* host: env('${process.env.PGHOST}', '127.0.0.1'),
-      port: env.int('${process.env.PGPORT}', 5432),
-      database: env('${process.env.PGDATABASE}', 'strapi'),
-      user: env('${process.env.PGUSER}', 'strapi'),
-      password: env('${process.env.PGPASSWORD}', 'password'), */
       host: env('PGHOST', '127.0.0.1'),
       port: env.int('PGPORT', 5432),
       database: env('PGDATABASE', 'strapi'),
